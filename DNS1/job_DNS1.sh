3a09b47adbe9af48f5cdc1267be893cb1a507201
mbjerg <31094333+mbjerg@users.noreply.github.com>
2018-07-01 15:14:11 +0200

    DNS1 case dir

---
+++ DNS1/job_DNS1.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#SBATCH --no-requeue
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=16
+#SBATCH --time=50:00:00
+#SBATCH --output=mpi_job_slurm.log
+#SBATCH --partition=xeon16
+
+source /home/mek/kmpan/OpenFOAM/OpenFOAM-4.1/etc/bashrc
+cd /home/mek/s153289/OpenFOAM/s153289-4.1/run/DNS1
+
+gmshToFoam mesh_DNS1.msh
+checkMesh
+mapFields ../boxTurb
+decomposePar
+mpirun -np 16 pisoFoam -parallel
+reconstructPar
+postProcess -funcs '(Ur5x Ur5y Ur15x Ur15y Ur20x Ur20y Ur25x Ur25y Ur30x Ur30y Ur35x Ur35y Ur40x Ur40y Uc)'
+
